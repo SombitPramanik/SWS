db437ce9788ebec5a04dc55bb58b8f95de2eec90
Sombit Pramanik <m.sombitpramanik@gmail.com>
2023-09-29 19:02:30 +0530

    add the main section

--- index.php
+++ index.php
@@ -4,7 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="language" content="en">
+    <meta name="description" content="Discover reliable server and hosting solutions tailored to your needs at [Your Website Name]. We specialize in delivering cutting-edge hosting services, including shared, VPS, and dedicated hosting, ensuring your website's optimal performance. Take advantage of our expertise in custom server configurations, allowing you to build a server environment that perfectly matches your business requirements. Additionally, we offer advanced storage solutions, empowering you with scalable and secure data management. Explore our range of cost-effective plans and enhance your online presence with seamless server solutions">
+    <meta name="keywords" content="web services, amazon web services, server,sql server,free website hosting, free domain and hosting, hosting, development server, sombit web services,https://hosting.sombti-server.online,">
+    <meta name="author" content="Sombit Pramanik">
+    <meta property="og:title" content="Sombit Web Services">
+    <meta property="og:description" content="Discover reliable server and hosting solutions tailored to your needs at Sombit Web Services. We specialize in delivering cutting-edge hosting services, including shared, VPS, and dedicated hosting, ensuring your website's optimal performance. Take advantage of our expertise in custom server configurations, allowing you to build a server environment that perfectly matches your business requirements. Additionally, we offer advanced storage solutions, empowering you with scalable and secure data management. Explore our range of cost-effective plans and enhance your online presence with seamless server solutions.">
+    <meta property="og:image" content="/IMG/sws.png">
+    <meta property="og:url" content="https://sombti-server.online">
+    <meta property="og:type" content="website">
+    <link rel="icon" href="/IMG/sws.ico" type="image/x-icon">
     <title>Sombit Web Services</title>
+
     <link rel="stylesheet" href="/CSS/style.css">
     <link rel="stylesheet" href="/CSS/responsive.css">
     <!-- <link rel="stylesheet" href="/CSS/debug.css"> -->
@@ -12,7 +23,8 @@
 </head>
 
 <body>
-    <?php include('./header.html');?>
+    <?php include('./header.html'); ?>
+    <?php include('./main.html'); ?>
 
 </body>
 <script src="/JS/nav_menu.js"></script>
