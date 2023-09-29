65810bd395bf293c511f4907b681370ecbe3a5bb
Sombit Pramanik <m.sombitpramanik@gmail.com>
2023-09-29 12:26:55 +0530

    create the base structre && full responsive NaviGation bar

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sombit Web Services</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="responsive.css">
-    <!-- <link rel="stylesheet" href="debug.css"> -->
+    <link rel="stylesheet" href="/CSS/style.css">
+    <link rel="stylesheet" href="/CSS/responsive.css">
+    <!-- <link rel="stylesheet" href="/CSS/debug.css"> -->
     <!-- This css is only for the DeBugging  -->
 </head>
 
