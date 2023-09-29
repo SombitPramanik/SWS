af5a06032499fade9316660bd239ab9327af877d
Sombit Pramanik <m.sombitpramanik@gmail.com>
2023-09-29 13:39:42 +0530

    playing with some php trickes

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php
-require ('./config.php');
-include ('./header.html');
+<!DOCTYPE html>
+<html lang="en">
 
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sombit Web Services</title>
+    <link rel="stylesheet" href="/CSS/style.css">
+    <link rel="stylesheet" href="/CSS/responsive.css">
+    <!-- <link rel="stylesheet" href="/CSS/debug.css"> -->
+    <!-- This css is only for the DeBugging  -->
+</head>
+
+<body>
+    <?php include('./header.html');?>
+
+</body>
+<script src="/JS/nav_menu.js"></script>
+
+</html>
